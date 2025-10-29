d3f5a14d9b55d8c3e3a6bb57861fd55ca975ad75
lynndixon <lynn@redhat.com>
2025-10-29 13:22:19 -0400

    Ch2-L3

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,20 +1,20 @@
-
-
-class HTMLNode():
-    def __init__(self, tag=None, value, children=None, props=None):
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
-        self.children = children if children is not None else []
-        self.props = props if props is not None else {}
+        self.children = children
+        self.props = props
 
     def to_html(self):
         raise NotImplementedError("to_html method not implemented")
-    
+
     def props_to_html(self):
-        props_str = ""
-        for key, value in self.props.items():
-            props_str += f' {key}="{value}"'
-        return props_str
+        if self.props is None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
 
     def __repr__(self):
-        return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
